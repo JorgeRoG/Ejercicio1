a56f1d7a4a469cb374edadf249536d6637de26bf
Jorge Romero <jlrg2905@gmail.com>
2020-09-06 16:41:20 -0500

    primer commit

---
+++ src/sample/Tienda.java
@@ -0,0 +1,34 @@
+package sample;
+
+public class Tienda{
+float precio;
+string descripcion;
+}
+
+public class Celular{
+string em;
+int IMEI;
+string color;
+string compania;
+string modelo;
+}
+
+public class weareble{
+int ns;
+string modelo;
+string color;
+
+}
+
+public class tablet{
+string marca;
+string modelo;
+int ns;
+string color;
+strng interfaz;
+}
+
+
+
+
+
